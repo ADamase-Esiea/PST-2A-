6dcf1d967e2447fe9d250c139ea49f49d41bb5b9
Pixon <revoil@et.esiea.fr>
2015-02-10 11:01:33 +0100

    Update Jeu.c

--- Jeu.c
+++ Jeu.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 int main () {
+	
 int TabJeu[14][14]; // Tableau de jeu (Il faudra change le type de variable par celui qu'on aura définis)
 
-//Bonjour Marc.
-
+//CONNARD DE GITHUB
 
 	return 0;
 }
