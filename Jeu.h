e1fc9e73224845806784f21094aaef0ebc4f5f13
Pixon <revoil@et.esiea.fr>
2015-02-10 11:17:08 +0100

    Update Jeu.h
    Ajout du type de structure Plateau contenant boule , tirette Horizontale et tirette verticale.

--- Jeu.h
+++ Jeu.h
@@ -7,7 +7,11 @@
 #include <stdlib.h>
 
 // Structure:
-
+struct Plateau {
+  int boule // 1 si présence de bille  // 0 si pas de bille 
+  int tireH // tirette horizontale 1 si présence de trou // 0 si pas de trou
+  int tireV // tierette verticale 1 si présence de trou // 0 si pas de trou
+}
 
 // Prototypes :
 
