ddc9f86666af73add41a1565834e27c466ba5884
Pixon <revoil@et.esiea.fr>
2015-02-12 23:32:46 +0100

    Update Jeu.c

--- Jeu.c
+++ Jeu.c
@@ -5,63 +5,63 @@
 
 
 void InitBilleArray(int TabJeu[14][14][3]) { // Initialise tout le plateau a 0 donc aucun billes elles seront placée par l'utilisateur au début du jeu après que les trous aient été placé aléatoirement.
-int i;	
-int j;
+	int i;	
+	int j;
 	for (i=0 ; i<14;i++) {
 		for (j=0;i<14;j++) {
-		TabJeu[i][j][0]=0;
-		TabJeu[i][j][1]=0;
-		TabJeu[i][j][2]=0;
+			TabJeu[i][j][0]=0;
+			TabJeu[i][j][1]=0;
+			TabJeu[i][j][2]=0;
 		}	
 	}	
 }
 
-void random(int TabJeu[14][14][3]) {
-int case1=0;
-int case2=0;
-int i=0;
-int j=0;
+void Alea(int TabJeu[14][14][3]) {
+	int case1=0;
+	int case2=0;
+	int i=0;
+	int j=0;
 
-Srand(time(NULL));
+	srand(time(NULL));
 	for (j=0;j<14;j++) {
 		for (i=0;i<14;i++){
 			if (i==1 || i==2) {
 				TabJeu[i][j][2]=rand()%2;
-}
+			}
 			else {
-case1=TabJeu[i-2][j][2];
-case2=TabJeu[i-1][j][2];
-			if (case1==0 && case2==0) {
-			TabJeu[i][j][2]=1;
-}
-			else { 
-			TabJeu[i][j][2]=rand()%2;
-	
-	
-}	
-}
+				case1=TabJeu[i-2][j][2];
+				case2=TabJeu[i-1][j][2];
+				if (case1==0 && case2==0) {
+					TabJeu[i][j][2]=1;
+				}
+				else { 
+					TabJeu[i][j][2]=rand()%2;
 
-				Printf("%d\n",TabJeu[i][j][2]);
 
-}
-}
+				}	
+			}
+
+			printf("%d\n",TabJeu[i][j][2]);
+
+		}
+	}
 }	
 
 
 
 void InitTabJeu(int TabJeu[14][14][3]) { // Fonction qui initialise le plateau de jeu avant le départ de la partie.
-	InitBilleArrat(TabJeu);
-	random(TabJeu);
-	
-	
+	InitBilleArray(TabJeu);
+	Alea(TabJeu);
+
+
 }
 
 
 int main () {
-	
-int TabJeu[14][14][3]; // Tableau de variables de type tableau contenant comme informations bille[0] , tireH [1] et tireV [2]
 
-InitTabJeu(TabJeu);
+	int TabJeu[14][14][3]; // Tableau de variables de type tableau contenant comme informations bille[0] , tireH [1] et tireV [2]
+
+	InitTabJeu(TabJeu);
 
 	return 0;
 }
