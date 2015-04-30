57ce29031edfce29be35206dc42edb818c668c84
Pixon <revoil@et.esiea.fr>
2015-04-30 20:37:00 +0200

    Update Jeu.c
    Bugué après modification fonction affichage

--- Jeu.c
+++ Jeu.c
@@ -6,20 +6,20 @@
 
 // Test Github
 void InitArray(int TabJeu[14][14][3]) { // Initialise tout le plateau a 0 donc aucun billes elles seront placée par l'utilisateur au début du jeu après que les trous aient été placé aléatoirement.
-	int i;	
+	int i;
 	int j;
 	for (i=0 ; i<14;i++) {
 		for (j=0;j<14;j++) {
 			TabJeu[i][j][0]=0;
 			TabJeu[i][j][1]=9;
 			TabJeu[i][j][2]=9;
-		}	
-	}	
+		}
+	}
 }
 
 void InitHorTirret(int TabJeu[14][14][3]) {
 
-	printf("\n***--Initialisation des Tirettes Horizontales--***\n");
+	printf("***--Initialisation des Tirettes Horizontales--***\n");
 
 	int case1=0;
 	int case2=0;
@@ -41,25 +41,25 @@ void InitHorTirret(int TabJeu[14][14][3]) {
 					if (case1==0 && case2==0) {
 						TabJeu[i][j][2]=1;
 					}
-					else { 
+					else {
 						TabJeu[i][j][2]=rand()%2;
-					}	
+					}
 				}
 
 			}
-		}}	
-	for (j=0;j<14;j++) 
+		}}
+	for (j=0;j<14;j++)
 	{
-		for (i=0;i<14;i++)	
+		for (i=0;i<14;i++)
 		{
-			printf("%d",TabJeu[i][j][2]);
+			//printf("%d",TabJeu[i][j][2]);
 		}
-		printf("\n");		
+		//printf("\n");
 	}
-}	
+}
 
 void InitVertTirret(int TabJeu[14][14][3]) {
-	printf("\n***--Initialisation des Tirettes Verticales--***\n");
+	printf("***--Initialisation des Tirettes Verticales--***\n");
 	int case1=0;
 	int case2=0;
 
@@ -81,15 +81,15 @@ void InitVertTirret(int TabJeu[14][14][3]) {
 					if (case1==0 && case2==0) {
 						TabJeu[i][j][1]=1;
 					}
-					else { 
+					else {
 						TabJeu[i][j][1]=rand()%2;
-					} 
+					}
 				}}
 
-				printf("%d",TabJeu[i][j][1]);
+				//printf("%d",TabJeu[i][j][1]);
 
 			}
-		} printf("\n");
+		} //printf("\n");
 	}
 }
 
@@ -113,16 +113,16 @@ int test;
 
 	printf("Choisissez la postion de la bille %d du joueur %d sur l'axe X\n",j,i);
 			while (x<2 || x>11) {
-				
+
 			scanf("%d",&x);
 			if (x<2 || x>11) {
 			printf("La valeur ne peux pas être inférieur a 2 ou dépasser 11\n");
 }
 
-			
+
 }
-			
-	
+
+
 
 	printf("Choisissez la position  de la bille %d du joueur %d sur l'axe Y\n", j ,i);
 		while (y<2 || y > 11) {
@@ -146,175 +146,156 @@ int test;
 }
 
 
+
 void ShowArray(int TabJeu[14][14][3])
 {
-	printf("\n***--Affichage du tableau de Jeu--***\n");
+	//on considere la tirette verticale comme la tirrete superieure
+
+	printf("***--Affichage du tableau de Jeu--***\n");
+	printf("Voici les notations :\n");
+	printf("- '*' signifie qu'il y a une bille\n");
+	printf("- 'X' signifie que vu du dessus il n'y a pas de trou\n");
+	printf("- 'P' signifie qu'il y a un trou seulement sur la premire couche\n");
+	printf("- 'B' signifie qu'il a un trou sur chaque couche\n");
 	int i=0,j=0;
-	couleur("32");
-	printf("|XX|-|A |B |--|C |D |E |F |G |H |I |J |K |L |--|M |N |\n");
+	couleur("36");
+	printf("| ** | A | B |---| C | D | E | F | G | H | I | J | K | L |---| M | N |\n");
 	couleur("0");
 
 	for(j=0;j<14;j++)
 	{
-		couleur("32");
-		if (j<10) {
-			printf("|%d |-",j);
-		}
-		else { 
-			printf("|%d|-",j);
-		}
-		couleur("0");
-
-		for(i=0;i<14;i++)
+		if(j==2 || j==12)
+		        {
+			        couleur("36");
+				        printf("|----|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|\n");
+					        couleur("0");
+						        }
+	if(j<10)
 		{
-			if(i==0)
-				printf("|");
-			if(i==2)
-			{
-				if(TabJeu[i][j][1]==9 && TabJeu[i][j][2]==9)
-				{   
-					couleur("32");                                
-					printf("--| #|");
-					couleur("0");
-				}           
-				if(TabJeu[i][j][0]==0)
-				{   
-					//Disons que Vertical supérieur)
-					if(TabJeu[i][j][2]==0)
-					{   
-						printf("--| N|");
-					}           
-					if(TabJeu[i][j][2]==1 && TabJeu[i][j][1]==9){                                                printf("--| T|");}
-					if(TabJeu[i][j][2]==9)
-					{
-						if(TabJeu[i][j][1]==1)
-							printf("--| T|");  
-						if(TabJeu[i][j][1]==0)
-							printf("--| 0|");
-
-					}   
-					if(TabJeu[i][j][2]==1)
-					{
-						if(TabJeu[i][j][1]==0)
-						{
-							printf("--| 0|");
-						}   
-						if(TabJeu[i][j][1]==1)
-						{
-							printf("--| T|");
-						}   
-					}                   
-				}                   
-				if(TabJeu[i][j][0]==1)
-				{
-					printf("--| *|");
-				}} else{
-					if(i==12)
-					{
-						if(TabJeu[i][j][1]==9 && TabJeu[i][j][2]==9)
-						{
-							couleur("32");
-							printf("--| #|");
-							couleur("32");
-						}
-						if(TabJeu[i][j][0]==0)
-						{
-							//Disons que Vertical supérieur)
-							if(TabJeu[i][j][2]==0)
-							{   
-								printf("--| N|");
-							}
-							if(TabJeu[i][j][2]==1 && TabJeu[i][j][1]==9){                                                printf("--| T|");}
-							if(TabJeu[i][j][2]==9)
-							{
-								if(TabJeu[i][j][1]==1)
-									printf("--| T|");
-								if(TabJeu[i][j][1]==0)
-									printf("--| 0|");
-
-							}
-							if(TabJeu[i][j][2]==1)
-							{
-								if(TabJeu[i][j][1]==0)
-								{
-									printf("--| 0|");
-								}
-								if(TabJeu[i][j][1]==1)
-								{
-									printf("--| T|");
-								}
-							}
-						}
-						if(TabJeu[i][j][0]==1)
-						{
-							printf("--| *|");
-
-						}}		
-					else{
-
-						if(TabJeu[i][j][1]==9 && TabJeu[i][j][2]==9)
-						{
-							couleur("32");
-							printf(" #|");
-							couleur("0");
-						}	
-						if(TabJeu[i][j][0]==0)
-						{
-							//Disons que Vertical supérieur)
-							if(TabJeu[i][j][2]==0)
-							{
-								printf(" N|");
-							}	
-							if(TabJeu[i][j][2]==1 && TabJeu[i][j][1]==9){
-								printf(" T|");}
-							if(TabJeu[i][j][2]==9)
-							{
-								if(TabJeu[i][j][1]==1)
-									printf(" T|");	
-								if(TabJeu[i][j][1]==0)
-									printf(" 0|");
-
-							}
-							if(TabJeu[i][j][2]==1)
-							{
-								if(TabJeu[i][j][1]==0)
-								{
-									printf(" 0|");
-								}
-								if(TabJeu[i][j][1]==1)
-								{
-									printf(" T|");
-								}
-							}
-						}
-						if(TabJeu[i][j][0]>0)
-						{
-							printf(" *|");
-						}}}	
-
-		}	if(j==1||j==11)
-		{
-			couleur("32");
-
-			printf("\n|--|-|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|--|\n");
+			couleur("36");
+			printf("| 0%d |",j);
 			couleur("0");
 		}
 		else{
-			printf("\n");
-		}	}
-}		
+			couleur("36");
+			printf("| %d |",j);
+			couleur("0");
+		}
+
+		for(i=0;i<14;i++)
+		{
+			// ON ENTRE DANS LE CAS DANS BILLE ------------------------------------------
+			if(i==2 || i==12)
+			{	couleur("36");
+				printf("---|");
+				couleur("0");
+			}
+
+			if(TabJeu[i][j][0]==0)
+			{
 
 
-//N signifie Pas de Trou
-//0 signifie Trou Vertical Et Horizontal Rempli
-//T signifie Trou Horizontal + Vertical
-//* signifie BILLE
-//int bille; // 1 si présence de bille  // 0 si pas de bille 
-//int tireH; // tirette horizontale 1 si présence de trou // 0 si pas de trou
-//int tireV; // tierette verticale 1 si présence de trou // 0 si pas de trou
+				if(TabJeu[i][j][1]==0)
+				{
+
+					if(TabJeu[i][j][2]==0)
+					{
+						printf(" B ");
+					}
+					if(TabJeu[i][j][2]==1)
+					{
+						printf(" P ");
+					}
+					if(TabJeu[i][j][2]==9)
+					{
+						printf(" P ");
+					}
+				}
+				if(TabJeu[i][j][1]==1)
+				{
+					if(TabJeu[i][j][2]==0)
+					{
+						printf(" X ");
+					}
+					if(TabJeu[i][j][2]==1)
+					{
+						printf(" X ");
+					}
+					if(TabJeu[i][j][2]==9)
+					{
+						printf(" X ");
+					}
+				}
+				if(TabJeu[i][j][1]==9)
+				{
+					if(TabJeu[i][j][2]==0)
+					{
+						printf(" P ");
+					}
+					if(TabJeu[i][j][2]==1)
+					{
+						printf(" X ");
+					}
+					if(TabJeu[i][j][2]==9)
+					{
+					printf(" # ");
+					}
+				}
+
+				couleur("36");
+				printf("|");
+				couleur("0");
+
+			}
+			// ON SORT SUR CAS SANS BILLE -----------------------------------------------
+
+			// ON ENTRE DANS LE CAS AVEC BILLE ------------------------------------------
+			else
+			{
+				if(TabJeu[i][j][0]==1)
+				{
+					couleur("32");
+					printf(" * ");
+					couleur("0");
+				}
+				if(TabJeu[i][j][0]==2)
+				{
+					couleur("33");
+					printf(" * ");
+					couleur("0");
+				}
+				if(TabJeu[i][j][0]==3)
+				{
+					couleur("34");
+					printf(" * ");
+					couleur("0");
+				}
+				if(TabJeu[i][j][0]==4)
+				{
+					couleur("35");
+					printf(" * ");
+					couleur("0");
+				}
+				couleur("36");
+				printf("|");
+				couleur("0");
+			}
+			// ON SORT DU CAS AVEC BILLE ------------------------------------------------
+
+		}
+		printf("\n");
+	}
+	couleur("36");
+	printf("|----|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|\n");
+	couleur("0");
+
+}
+
+
+
 void InitTabJeu(int TabJeu[14][14][3]) { // Fonction qui initialise le plateau de jeu avant le départ de la partie.
 int nbPlayer=2;
-int nbBilles=2;
+int nbBilles=1;
 
 	InitArray(TabJeu);
 	InitVertTirret(TabJeu);
